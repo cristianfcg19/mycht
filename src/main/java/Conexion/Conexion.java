0d8f14fd5f3fb7d42b5c85ae2a0a0092f2361ac6
David Alfaro <alfardavid07@gmail.com>
2022-08-19 00:26:35 -0600

    commit threads

--- src/main/java/Conexion/Conexion.java
+++ src/main/java/Conexion/Conexion.java
@@ -29,7 +29,7 @@ public class Conexion
         try
         {
         Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-        connection= DriverManager.getConnection(DBurl,"cristian","1234");
+        connection= DriverManager.getConnection(DBurl,"root","caca123X!");
         } catch (ClassNotFoundException |SQLException|NoSuchMethodException|
                 SecurityException|InstantiationException|IllegalAccessException
                 |IllegalArgumentException|InvocationTargetException exception)
